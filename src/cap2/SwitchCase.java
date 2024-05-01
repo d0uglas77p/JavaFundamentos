7c5cf173416fb5d19851d2fb0a6194f930aa8dd1
d0uglas77p <douglas_1313@hotmail.com>
2024-05-01 16:36:31 -0300

    switch case

---
+++ src/cap2/SwitchCase.java
@@ -0,0 +1,26 @@
+package cap2;
+
+public class SwitchCase {
+    public static void main(String[] args) {
+
+        String sigla = "P";
+
+        switch (sigla) {
+            case "P": {
+                System.out.println("PEQUENO");
+                break;
+            }
+            case "M": {
+                System.out.println("MÉDIO");
+                break;
+            }
+            case "G": {
+                System.out.println("GRANDE");
+                break;
+            }
+            default:
+                System.out.println("INDEFINIDO");
+        }
+    }
+
+}
